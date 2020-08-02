d594e7f8cd54697a2e01c3e5acd2a08798cc1c3e
Visual Studio 2015 <fiv15184@ploae.com>
2020-08-03 00:49:02 +0300

    generate hash sha-256 for password string

--- src/main/java/ru/progwards/java2/lib/IDbTable.java
+++ src/main/java/ru/progwards/java2/lib/IDbTable.java
@@ -1,8 +1,17 @@
 package ru.progwards.java2.lib;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.function.Predicate;
 
+/**
+ * Интерфейс для таблиц БД,
+ * включает статический метод {@link #hashSha256(String)} для получения хеша по паролю
+ * @param <K> ключ
+ * @param <V> значение
+ */
 public interface IDbTable<K, V> {
     String getTableName();
 
@@ -16,4 +25,32 @@ public interface IDbTable<K, V> {
 
     void readAll() throws Exception;
     void saveAll() throws Exception;
+
+    /**
+     * Метод для получения хеша из строки по алгоритму SHA-256
+     * @param stringToEncrypt исходная строка (например, пароль)
+     * @return хеш-код в виде строки байт (16x представление) с лидирующими нулями
+     */
+    static String hashSha256(String stringToEncrypt) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(stringToEncrypt.getBytes(StandardCharsets.UTF_8));
+            return bytesToHex(messageDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        if (hash == null)
+            return null;
+        StringBuffer hexString = new StringBuffer();
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
 }
